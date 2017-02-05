83d6fa11265a64a0847a63f9c64dfd9f984bce91
zanakoliqi <col_zana@hotmail.com>
2017-02-05 15:15:23 +0100

    templates and css are added.

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/')
 def index():
-	return "Flask Hello World"
+	return render_template("index.html")
+
+@mod_main.route('/form')
+def form():
+	name="Techstitution"
+	return render_template("form.html",name=name)
