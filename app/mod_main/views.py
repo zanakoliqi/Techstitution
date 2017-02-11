8a219ca5178d0a59c8057cbb3b9ec075c2ba4b14
zanakoliqi <col_zana@hotmail.com>
2017-02-11 11:07:33 +0100

    Remove unneccesary files

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint,render_template
+from app import mongo
 
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/')
 def index():
+	db =mongo.db.arkep
+	db.insert({"name": "arkep"})
 	return render_template("index.html")
 
 @mod_main.route('/form')
